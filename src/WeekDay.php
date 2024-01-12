6734846b47c3064d57aec26b6b9e43bb14dfbca8
Tyler Lartonoix <tyler@lartonoix.com>
2024-01-12 15:02:56 -0500

    WIP
    There's a lot in this commit. Context building has been unified,
    template file extensions have been shortened to `hbs`, and the WeekDay
    contract has been added. Additionally, enum templates have been updated
    to allow for backing variants with any value (though in practice this
    should only ever be a string or an integer).

---
+++ src/WeekDay.php
@@ -0,0 +1,12 @@
+<?php
+	namespace Gravitas\Orbit\Contracts;
+
+	enum WeekDay: int {
+		case Sunday = 0;
+		case Monday = 1;
+		case Tuesday = 2;
+		case Wednesday = 3;
+		case Thursday = 4;
+		case Friday = 5;
+		case Saturday = 6;
+	}
