e9bd29672cb2e59fe3187e867002e0ac9464de52
Tyler Lartonoix <tyler@lartonoix.com>
2024-01-17 10:11:16 -0500

    Build contracts

--- src/JwtClaim.php
+++ src/JwtClaim.php
@@ -14,11 +14,17 @@
 		// A unix timestamp indicating when the token becomes valid.
 		public const NotBefore = "nbf";
 
+		// A unique identifier for a user, such as an email address for regular users or a service's name for service users.
+		public const UserIdentifier = "userIdentifier";
+
 		// The ID of the account the user belongs to; not present for service users.
 		public const Account = "accountId";
 
 		// An array of permission strings indicating what actions the user may perform; not present for service users.
 		public const Permissions = "permissions";
+
+		// An array of firewall roles indicating what app-level actions a user may perform.
+		public const Roles = "roles";
 
 		private function __construct() {}
 	}
